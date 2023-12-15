6d0be02519a41cb7458b968297027d7a6517d368
rossmagp <rossmagp@gmail.com>
2023-12-15 15:55:43 -0500

    added sprites

--- scripts/script.js
+++ scripts/script.js
@@ -27,35 +27,41 @@ function displayPokemon(event, pokemonData) {
 
     pokemonInfoList.innerHTML = "";
 
+    // id
     const id = document.createElement("li");
     id.innerHTML = `<label>ID:</label> ${pokemonData.id}`;
 
+    //sprite
+    const sprite = document.createElement("li");
+    id.innerHTML = `<img src="${pokemonData.sprites.front_default}"></img>`;
+
+    // name
     const name = document.createElement("li");
     name.innerHTML = `<label>Name:</label> ${pokemonData.name}`;
 
-    const baseExperience = document.createElement("li");
-    baseExperience.innerHTML = `<label>Base Experience:</label> ${pokemonData.base_experience}`;
-
+    // weight
     const weight = document.createElement("li");
     weight.innerHTML = `<label>Weight:</label> ${pokemonData.weight} kg`;
 
+    // height
     const height = document.createElement("li");
     height.innerHTML = `<label>Height:</label> ${pokemonData.height} m`;
 
+    // abilities
     const abilities = document.createElement("li");
     abilities.innerHTML = `<label>Abilities:</label> ${pokemonData.abilities
         .map((ability) => ability.ability.name)
         .join(', ')}`;
 
+    // moves    
     const moves = document.createElement("li");
     moves.innerHTML = `<label>Moves:</label> ${pokemonData.moves
         .map((move) => move.move.name)
-        .slice(0,5)
+        .slice(0, 5)
         .join(', ')}`;
 
     pokemonInfoList.appendChild(id);
     pokemonInfoList.appendChild(name);
-    pokemonInfoList.appendChild(baseExperience);
     pokemonInfoList.appendChild(weight);
     pokemonInfoList.appendChild(height);
     pokemonInfoList.appendChild(abilities);
